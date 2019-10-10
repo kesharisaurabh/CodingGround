831281bba24b2915d2106fac95765609ae6d1db8
SAURABH KESHARI <keshari.saurabh@yahoo.in>
2019-10-10 13:08:41 +0530

    Add files via upload

---
+++ Serialization/student.java
@@ -0,0 +1,11 @@
+package Serialization;
+
+import java.io.Serializable;
+
+public class student implements Serializable
+{
+	public final static long serialVersionUID=1L;
+	
+	int sid;
+	String name;
+}
